6d31c2675bd77a260906c3f0aebb6d2e20e1faee
takuyaokabe@gmail.com <takuyaokabe@gmail.com>
2025-11-03 23:12:44 +0900

    comments

--- env.sh
+++ env.sh
@@ -4,11 +4,11 @@ export SOURCEDIR="$HOME/docker-mirakurun-epgstation/recorded/" # m2ts (変換前
 OUTDIR="$HOME/media/" # mp4 (変換後) の出力先ディレクトリ
 
 # オプション
-CMCUT=false   # CMカットしないなら false
-CHKDRP=false #動画エラーチェックしないなら false
+CMCUT=false   # false: CMカットなし
+CHKDRP=false # false: 動画チェックなし
 export WATCHDIR=false # false: サーバー(EPGSTATION_URL)監視、true: ディレクトリ(SOURCEDIR) 監視
-export EPGSTATION_URL="http://localhost:8888/" #ディレクトリ監視なら設定不要
-#NTFY_URL="" # ntfy.sh通知先： 未設定ならコメントアウト
-# NHKチャンネルID: CMCUT=falseなら設定不要
+export EPGSTATION_URL="http://localhost:8888/" #
+#NTFY_URL="" # ntfy.sh 未設定ならコメントアウト
+#NHKチャンネルID: CMCUT=falseなら設定不要
 #NHK1="GR26"
 #NHK2="GR27"
